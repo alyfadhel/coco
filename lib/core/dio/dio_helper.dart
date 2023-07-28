6b58bd83b123bb73f639e60e7d2ff479aa8f96f5
alyel <alyfadel67@gmail.com>
2023-07-28 16:40:43 +0100

    login

--- lib/core/dio/dio_helper.dart
+++ lib/core/dio/dio_helper.dart
@@ -1,57 +1,60 @@
 import 'package:dio/dio.dart';
 
 class DioHelper {
-
   static late Dio dio;
 
-  static init() async
-  {
-    dio = Dio(
-      BaseOptions(
-        baseUrl: 'https://student.valuxapps.com/api/',
-        receiveDataWhenStatusError: true,
-      )
+  static init() async {
+    dio = Dio(BaseOptions(
+      baseUrl: 'https://student.valuxapps.com/api/',
+      receiveDataWhenStatusError: true,
+    ));
+  }
+
+  static Future<Response> getData({
+    required String url,
+    Map<String, dynamic>? query,
+  }) async {
+    return await dio.get(
+      url,
+      queryParameters: query,
     );
   }
 
- static Future<Response> getData({
+  static Future<Response> putData({
     required String url,
-    Map<String, dynamic>?query,
-  }) async{
-   return await dio.get(url, queryParameters: query,);
-  }
-
- static Future<Response>putData({
-    required String url,
-    Map<String, dynamic>?query,
-    required Map<String, dynamic>data,
+    Map<String, dynamic>? query,
+    required Map<String, dynamic> data,
     String? lang,
     String? token,
-})async
-  {
+  }) async {
     dio.options.headers = {
-      'Content-Type' : 'application/json',
-      'lang' : lang,
-      'Authorization' : token ??'',
-
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? '',
     };
-   return await dio.put(url,queryParameters: query,data: data,);
+    return await dio.put(
+      url,
+      queryParameters: query,
+      data: data,
+    );
   }
 
- static Future<Response>postData({
+  static Future<Response> postData({
     required String url,
-    Map<String, dynamic>?query,
-    required Map<String, dynamic>data,
+    Map<String, dynamic>? query,
+    required Map<String, dynamic> data,
     String? lang,
     String? token,
-  })async
-  {
+  }) async {
     dio.options.headers = {
-      'Content-Type' : 'application/json',
-      'lang' : lang,
-      'Authorization' : token ??'',
-
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? '',
     };
-    return await dio.post(url,queryParameters: query,data: data,);
+    return await dio.post(
+      url,
+      queryParameters: query,
+      data: data,
+    );
   }
 }
